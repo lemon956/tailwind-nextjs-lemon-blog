41df6077b8686bcc0039e487eb96002b589b6f9f
gerry <gerry@hellotalk.com>
2025-05-15 11:13:07 +0800

    feat: update site url and repo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: "Lemon's personal blog website",
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://lemon9527.top',
+  siteRepo: 'https://github.com/lemon956/tailwind-nextjs-lemon-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
